618403f4e96ffb266c453ce5f343a0d6591cfacb
Rushabh Mehta <mehtarushabh2005@gmail.com>
2024-11-10 18:26:13 +0530

    change S.py

--- exp6/SOLID/S.py
+++ exp6/SOLID/S.py
@@ -1,17 +1,13 @@
 # S: Single Responsibility Principle
 class DataFetcher:
     """Handles data fetching responsibility."""
-
     def fetch_data(self):
         return [1, 2, 3, 4, 5]
 
-
 class DataProcessor:
     """Handles data processing responsibility."""
-
     def calculate_mean(self, data):
         return round(sum(data) / len(data), 3)
-
 
 # Usage
 fetcher = DataFetcher()
